972daffff67fa71af369559ddb94555b4b7c93f8
Armen Agabekian <2008nv@gmail.com>
2024-05-09 20:40:27 -0700

    bike-model-working-typo

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const { db } = require('./src/models');
 const server = require('./src/server.js');
 const PORT = process.env.PORT || 3001
 
+
+
 db.sync().then(() => {
   server.start(PORT);
 });
--- src/models/bike/model.js
+++ src/models/bike/model.js
@@ -5,7 +5,7 @@ const bikeModel = (sequelize, DataTypes) => sequelize.define('Bike', {
     model: {type: DataTypes.STRING, required: true},
     color: {type: DataTypes.STRING, required: true},
     price: {type: DataTypes.INTEGER, required: true},
-    type: {type: DataTypes.ENUM('road', 'mountain', 'hybrid', 'folding'), required: true}
+    type: {type: DataTypes.ENUM('road', 'mountain', 'hybrid', 'folding'), required: true},
     //status rare - vintage -etc
     userId: {
         type: DataTypes.INTEGER,
